b1679735f0938a7bee2d8e874c9525cfe00fcdca
“eulerex” <czhang4@my.hpu.edu>
2025-07-05 08:46:48 +0900

    Initial commit: Spring Boot order system project

---
+++ src/main/java/com/battery/ordersystem/BatteryOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.battery.ordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatteryOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatteryOrderSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/battery/ordersystem/BatteryOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.battery.ordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatteryOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
